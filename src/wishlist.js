1ff95bad1904c4c532580dc4b40768ba76e06cdd
reid <reidbras@gmail.com>
2021-10-08 21:34:21 -0500

    Updating logging

--- src/wishlist.js
+++ src/wishlist.js
@@ -1,7 +1,7 @@
 class wishlist {
     constructor(car) {
         console.log("---Begin wishlist constructor---");
-        console.log(`Constructor Parameters:\n1. ${car}`);
+        console.log(`Constructor Parameters:\n1. ${car.make}\n2. ${car.model}\n3. ${car.year}`);
         this.car = car;
     }
     add(car) {
