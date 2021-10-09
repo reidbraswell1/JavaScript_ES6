92125330ba4c6bf0fbc2cfd84b9657e1f35db874
reid <reidbras@gmail.com>
2021-10-08 21:10:05 -0500

    Adding logging

---
+++ src/car.js
@@ -0,0 +1,14 @@
+class car {
+    constructor (make, model, year) {
+        console.log("---Begin car constructor---");
+        console.log(`Constructor Parameters:\n1. make=${make}\n2. model=${model}\n3. year=${year} `)
+        this.make = make;
+        this.model = model;
+        this.year = year;
+    }
+    info() { 
+        console.log("---Begin info()---");
+        console.log(`Parameters:\n1. make=${this.make}\n2. model=${this.model} \n3. year=${this.year}`);
+    }
+}
+export { car };
