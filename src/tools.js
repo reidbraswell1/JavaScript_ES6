0add68f8f168d6b900985fa58f548afd28b935e2
Reid Braswell <reidbras@gmail.com>
2021-10-09 16:27:21 -0500

    Adding code to delete table row

--- src/tools.js
+++ src/tools.js
@@ -1,16 +1,34 @@
-export function displayWishList(obj) {
+//import { wishlist } from "./wishlist";
+
+export function displayWishList(wishList, obj) {
   console.log("---Begin displayWishList()---");
   const tr = document.createElement("tr");
   const tr_tbody = document.getElementById("wishlist-tbody");
   const tdMake = document.createElement("td");
   const tdModel = document.createElement("td");
   const tdYear = document.createElement("td");
+  const tdId = document.createElement("td");
+  const tdDelete = document.createElement("td");
   tdMake.innerText = obj.make;
   tdModel.innerText = obj.model;
   tdYear.innerText = obj.year;
+  tdDelete.innerHTML="<button id=" + `wishlist-delete-${wishList.length - 1}` + ">Delete</button>";
+  tdId.className = "hidden";
   tr.appendChild(tdMake);
   tr.appendChild(tdModel);
   tr.appendChild(tdYear);
+  tr.appendChild(tdId);
+  tr.appendChild(tdDelete);
   tr_tbody.appendChild(tr);
+  const tdDeleteButton = document.getElementById(`wishlist-delete-${wishList.length - 1}`);
+  tdDeleteButton.addEventListener('click',deleteRow);
   console.log("---End displayWishList()---");
+}
+
+function deleteRow() {
+    console.log("---Begin Delete Row---")
+    const stringArray = this.id.split("-");
+    const id = stringArray[stringArray.length - 1];
+    alert(`${this.id}, ${id}, ${this.innerText}`);
+    document.getElementById(`wishlist-row-${id}`).remove();
 }
