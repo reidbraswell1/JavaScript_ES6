a6fc1ade560ac52e5f6b33325c16ab44434c09ab
Reid Braswell <reidbras@gmail.com>
2021-10-09 12:43:25 -0500

    Updating add method and remove method for wishlist

--- src/wishlist.js
+++ src/wishlist.js
@@ -1,13 +1,15 @@
 class wishlist {
+    list = [];
+
     constructor(car) {
         console.log("---Begin wishlist constructor---");
         console.log(`Constructor Parameters:\n1. ${car.make}\n2. ${car.model}\n3. ${car.year}`);
         this.car = car;
     }
     add(car) {
-
+        this.list.push(car);
     }
-    remove(car) {
+    remove(idx,car) {
 
     }
 }
