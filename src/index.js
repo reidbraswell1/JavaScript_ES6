8dcb069816033fe7e25ad73377f9cd68baaf4e48
reid <reidbras@gmail.com>
2021-10-10 22:38:32 -0500

    Moving event listener callback functions to index.js

--- src/index.js
+++ src/index.js
@@ -6,3 +6,24 @@ let myCar = new car("myMake", "myModel", "myYear");
 myCar.info();
 let myWishlist = new wishlist([]);
 myWishlist.add(myCar);
+
+export function deleteRow() {
+    console.log("---Begin Delete Row---")
+    const stringArray = this.id.split("-");
+    const id = stringArray[stringArray.length - 1];
+    alert(`${this.id}, ${id}, ${this.innerText}`);
+    document.getElementById(`wishlist-row-${id}`).remove();
+    myWishlist.remove(id);
+}
+
+export function updateRow() {
+    console.log("---Begin Update Row---")
+    const stringArray = this.id.split("-");
+    const id = stringArray[stringArray.length - 1];
+    alert(`${this.id}, ${id}, ${this.innerText}`);
+    myWishlist.update(id);
+    /*
+    document.getElementById(`wishlist-row-${id}`).remove();
+    myWishlist.remove(id);
+    */
+}
