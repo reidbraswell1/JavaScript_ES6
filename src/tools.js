8662dd7b6102eecaef2e730db516128ff365dcff
reid <reidbras@gmail.com>
2021-10-10 22:40:28 -0500

    Adding id and moving callback event listener to index.js

--- src/tools.js
+++ src/tools.js
@@ -1,37 +1,34 @@
-//import { wishlist } from "./wishlist";
+import { deleteRow } from './index.js';
+import { updateRow } from './index.js';
+import { wishlist } from './wishlist';
 
 export function displayWishList(wishList, obj) {
   console.log("---Begin displayWishList()---");
   const tr = document.createElement("tr");
   const tr_tbody = document.getElementById("wishlist-tbody");
+  const tdId = document.createElement("td");
   const tdMake = document.createElement("td");
   const tdModel = document.createElement("td");
   const tdYear = document.createElement("td");
-  const tdId = document.createElement("td");
   const tdUpdate = document.createElement("td");
   const tdDelete = document.createElement("td");
+  tdId.innerText = wishList.length;
   tdMake.innerText = obj.make;
   tdModel.innerText = obj.model;
   tdYear.innerText = obj.year;
   tdUpdate.innerHTML="<button id=" + `wishlist-update-${wishList.length - 1}` + ">Update</button>";
   tdDelete.innerHTML="<button id=" + `wishlist-delete-${wishList.length - 1}` + ">Delete</button>";
-  tdId.className = "hidden";
+  tr.id = `wishlist-row-${wishlist.length - 1}`;
+  tr.appendChild(tdId);
   tr.appendChild(tdMake);
   tr.appendChild(tdModel);
   tr.appendChild(tdYear);
-  tr.appendChild(tdId);
   tr.appendChild(tdUpdate);
   tr.appendChild(tdDelete);
   tr_tbody.appendChild(tr);
+  const tdUpdateButton = document.getElementById(`wishlist-update-${wishList.length - 1}`);
+  tdUpdateButton.addEventListener('click',updateRow);
   const tdDeleteButton = document.getElementById(`wishlist-delete-${wishList.length - 1}`);
   tdDeleteButton.addEventListener('click',deleteRow);
   console.log("---End displayWishList()---");
-}
-
-function deleteRow() {
-    console.log("---Begin Delete Row---")
-    const stringArray = this.id.split("-");
-    const id = stringArray[stringArray.length - 1];
-    alert(`${this.id}, ${id}, ${this.innerText}`);
-    document.getElementById(`wishlist-row-${id}`).remove();
 }
