16ba4b7655e7e4606f303f014606a8ae364ec125
Reid Braswell <reidbras@gmail.com>
2021-10-17 15:54:20 -0500

    Moving test data to test.js

--- src/index.js
+++ src/index.js
@@ -4,22 +4,6 @@ import { Wishlist } from "./wishlist";
 
 // Test Data
 export let myWishlist = new Wishlist([]);
-/*
-for (let i=0; i < 300; i++) {
-    let myCar;
-    if(i < 10) {
-        myCar = new car(`myMake${i+1}`, `myModel${i+1}`, `myYear200${i+1}`);
-    }
-    else if(i+1 > 99) {
-        myCar = new car(`myMake${i+1}`, `myModel${i+1}`, `myYear2${i+1}`);
-    }
-    else {
-        myCar = new car(`myMake${i+1}`, `myModel${i+1}`, `myYear20${i+1}`);
-    }
-    myCar.info();
-    myWishlist.add(myCar);
-}
-*/
 
 export function controller(obj) {
     console.log("---Begin controller()---");
