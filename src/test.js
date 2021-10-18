787b769cab1188296fa1e62f20bc58efff00f899
reid <reidbras@gmail.com>
2021-10-17 21:49:15 -0500

    Refining gererateTestData function

--- src/test.js
+++ src/test.js
@@ -1,19 +1,18 @@
 import { car } from './car.js';
 import { Wishlist } from './wishlist';
 
-let myWishlist = new Wishlist([]);
-
 export function generateTestData(count) {
+    let myWishlist = new Wishlist([]);
     for (let i=0; i < count; i++) {
         let myCar;
         if(i < 10) {
-            myCar = new car(`myMake${i+1}`, `myModel${i+1}`, `myYear200${i+1}`);
+            myCar = new car(`make${i+1}`, `model${i+1}`, `year200${i+1}`);
         }
         else if(i+1 > 99) {
-            myCar = new car(`myMake${i+1}`, `myModel${i+1}`, `myYear2${i+1}`);
+            myCar = new car(`make${i+1}`, `model${i+1}`, `year2${i+1}`);
         }
         else {
-            myCar = new car(`myMake${i+1}`, `myModel${i+1}`, `myYear20${i+1}`);
+            myCar = new car(`make${i+1}`, `model${i+1}`, `year20${i+1}`);
         }
         myCar.info();
         myWishlist.add(myCar);
