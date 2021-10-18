24bf0f23c4c1845762cd34f672c80e6f66d9ebeb
reid <reidbras@gmail.com>
2021-10-18 13:14:28 -0500

    Moved code that populated table cells with car object information to the info method in the car object

--- src/tools.js
+++ src/tools.js
@@ -1,35 +1,10 @@
-//import { deleteRow } from './index.js';
-//import { updateRow } from './index.js';
-import { Wishlist } from './wishlist';
 import { myWishlist } from './index.js';
 
-export function displayWishList(wishList, obj) {
+export function displayWishList(wishList, car) {
   console.log("---Begin displayWishList()---");
-  const tr = document.createElement("tr");
-  const tr_tbody = document.getElementById("wishlist-tbody");
-  const tdId = document.createElement("td");
-  const tdMake = document.createElement("td");
-  const tdModel = document.createElement("td");
-  const tdYear = document.createElement("td");
-  const tdUpdate = document.createElement("td");
-  const tdDelete = document.createElement("td");
-  tdId.innerText = `${wishList.length}.`;
-  tdMake.innerText = obj.make;
-  tdModel.innerText = obj.model;
-  tdYear.innerText = obj.year;
-  tdMake.id = `td-make-${wishList.length - 1}`;
-  tdModel.id = `td-model-${wishList.length - 1}`;
-  tdYear.id = `td-year-${wishList.length - 1}`;
-  tdUpdate.innerHTML="<button id=" + `wishlist-update-${wishList.length - 1}` + ">Update</button>";
-  tdDelete.innerHTML="<button id=" + `wishlist-delete-${wishList.length - 1}` + ">Delete</button>";
-  tr.id = `wishlist-row-${wishList.length - 1}`;
-  tr.appendChild(tdId);
-  tr.appendChild(tdMake);
-  tr.appendChild(tdModel);
-  tr.appendChild(tdYear);
-  tr.appendChild(tdUpdate);
-  tr.appendChild(tdDelete);
-  tr_tbody.appendChild(tr);
+  // Populate the table cells with the car info
+  car.info(wishList);
+  // Create an event listener for update and delete events
   const tdUpdateButton = document.getElementById(`wishlist-update-${wishList.length - 1}`);
   tdUpdateButton.addEventListener('click',updateRow);
   const tdDeleteButton = document.getElementById(`wishlist-delete-${wishList.length - 1}`);
